b6b1d1faed23572815fe48b187fdc33c1be28bc3
satwaj <satwajbachhav2004@gmail.com>
2026-07-24 00:46:12 +0530

    added agent service

---
+++ Sandbox/agent/server.js
@@ -0,0 +1,7 @@
+import app from "./src/app.js"
+
+
+app.listen(3000, () => {
+  console.log("SAndbox Agent Server is running on port 3000")
+})
+
--- Sandbox/router/src/app.js
+++ Sandbox/router/src/app.js
@@ -17,6 +17,7 @@ app.get("/api/status/readyz",(req,res)=>{
 })
 
 const proxies = {}
+const agentProxies = {}
 
 function getProxy(sandboxId) {
 
@@ -33,18 +34,38 @@ function getProxy(sandboxId) {
 
 }
 
+function getAgentProxy(sandboxId) {
+
+    const target = `http://sandbox-service-${sandboxId}:3000`; // Construct target URL based on sandboxId
+
+    if (!agentProxies[ sandboxId ]) {
+        agentProxies[ sandboxId ] = createProxyMiddleware({
+            target,
+            changeOrigin: true,
+            ws: true,
+        })
+    }
+
+    return agentProxies[ sandboxId ];
+}
+
 app.use((req, res, next) => {
+    const host = req.headers.host;
+    const sandboxId = host.split('.')[ 0 ]; // Extract sandboxId from subdomain
 
-  const host = req.headers.host
-  const sandboxId = host.split('.')[0]
-  
+    /**
+     * pod1.preview.localhost
+     * pod1.agent.localhost
+     */
 
+    if (host.split('.')[ 1 ] === 'agent') {
+        return getAgentProxy(sandboxId)(req, res, next);
+    }
 
-  return getProxy(sandboxId)(req, res, next);
-
+    else if (host.split('.')[ 1 ] === 'preview') {
+        return getProxy(sandboxId)(req, res, next);
+    }
 })
 
-
-
-
 export default app
+
